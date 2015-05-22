8496dce266f92b4b6ce310d303f4f8cdac932fc0
jietan <jietan_001@hotmail.com>
2015-05-22 10:35:27 -0400

    add evaluation.cpp for compiling errors

---
+++ robot/Evaluation.cpp
@@ -0,0 +1,58 @@
+#include "Evaluation.h"
+
+#include "HumanoidController.h"
+#include "CMAData.h"
+#include "Motion.h"
+#include "dart/simulation/World.h"
+#include "dart/dynamics/Skeleton.h"
+
+
+double evalController(dart::simulation::World* world, bioloidgp::robot::HumanoidController* controller, CMAData* cData, int pop_id, double* timerPerStep)
+{
+	double reward = 0;
+	double motionTime = controller->motion()->getMotionLength();
+	const double testTime = motionTime + 1.0;
+	while (world->getTime() < testTime)
+	{
+		controller->update(world->getTime());
+		world->step();
+
+		Eigen::Vector3d up = controller->getUpDir();
+		Eigen::Vector3d left = controller->getLeftDir();
+
+		double asinNegativeIfForward = up.cross(Eigen::Vector3d::UnitY()).dot(left);
+		double angleFromUp = asin(asinNegativeIfForward);
+		double threshold = 60.0 * M_PI / 180.0;
+		if (abs(angleFromUp) > threshold)
+			break;
+		//LOG(INFO) << gWorld->getTime() << " " << angleFromUp;;
+
+		if (world->getTime() > motionTime)
+		{
+			reward += -1.0 / (abs(angleFromUp) + 0.1);
+		}
+		else
+		{
+			reward += -1;
+		}
+	}
+	return reward;
+
+}
+double evalSystemId(dart::simulation::World* world, bioloidgp::robot::HumanoidController* controller, CMAData* cData, int pop_id, double* timerPerStep)
+{
+	double reward = 0;
+	const double testTime = 1.4;
+	while (world->getTime() < testTime)
+	{
+		double t = world->getTime();
+		Eigen::VectorXd q = controller->robot()->getPositions();
+		double globalAngle = q.head(3).norm();
+		//LOG(INFO) << globalAngle;
+		controller->update(world->getTime());
+		world->step();
+		reward += controller->compareGlobalRotationWithReferenceTrajectories(t, globalAngle);
+	}
+
+	return reward;
+}
